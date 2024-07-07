bd078f846c7e95436950c034d20d2a718c76b3db
JahanZaib Malik <116706071+Zaib001@users.noreply.github.com>
2024-07-06 21:52:24 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,33 +1,27 @@
 const mongoose = require("mongoose");
+
 module.exports = () => {
-  const URL = "mongodb+srv://zebimalik4567:Miral1234@cluster0.3mdfkeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-  mongoose.connect(URL);
+  const URL = "mongodb+srv://zebimalik4567:Miral1234@cluster0.3mdfkeu.mongodb.net/mydatabase?retryWrites=true&w=majority";
+  mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
   
   const db = mongoose.connection;
 
- 
   db.once("open", () => {
     console.log("Connected to MongoDB");
-    return "Connected to MongoDB";
   });
 
-  // On connection error
   db.on("error", (err) => {
-    console.log("Error connecting to MongoDB:", err);
-    return "Error connecting to MongoDB:", err;
+    console.error("Error connecting to MongoDB:", err);
   });
 
-  // On connection disconnection
   db.on("disconnected", () => {
     console.log("Disconnected from MongoDB");
-    return "Disconnected from MongoDB";
   });
 
-  // On process termination, close the connection
   process.on("SIGINT", () => {
     mongoose.connection.close(() => {
       console.log("MongoDB connection disconnected through app termination");
-      return "MongoDB connection disconnected through app termination";
+      process.exit(0);
     });
   });
 };
