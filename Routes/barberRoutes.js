ad4bc1eb5fb53e219786278b4593a2ff654f2ca7
JahanZaib Malik <116706071+Zaib001@users.noreply.github.com>
2024-10-29 00:28:15 -0700

    Update barberRoutes.js

--- Routes/barberRoutes.js
+++ Routes/barberRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createBarber, getBarbers, getBarberById, deleteBarber } = require("../controllers/barberController");
+const { createBarber, getBarbers, getBarberById, deleteBarber,updateBarberStatus } = require("../controllers/barberController");
 
 router.post("/", createBarber);
 router.get("/", getBarbers);
 router.get('/:id', getBarberById);
 router.delete('/:id', deleteBarber);  // Add this line for the delete route
-
+router.patch('/:id', updateBarberStatus);
 module.exports = router;
