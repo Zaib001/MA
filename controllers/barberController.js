8badfd0d8a4373cf1bda44f298442bdb4367efb5
JahanZaib Malik <116706071+Zaib001@users.noreply.github.com>
2024-11-22 12:09:28 -0800

    Update barberController.js

--- controllers/barberController.js
+++ controllers/barberController.js
@@ -5,7 +5,6 @@ exports.createBarber = async (req, res) => {
 
   try {
     const barber = await Barber.create({ name, email, description, specialty, image, status });
-    console.log(barber)
     res.status(201).json(barber);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -16,7 +15,6 @@ exports.createBarber = async (req, res) => {
 exports.getBarbers = async (req, res) => {
   try {
     const barbers = await Barber.find();
-    console.log(barbers)
     res.status(200).json(barbers);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -50,12 +48,12 @@ exports.deleteBarber = async (req, res) => {
 };
 exports.updateBarberStatus = async (req, res) => {
   const { id } = req.params;
-  const { status } = req.body;
+  const { status, availability } = req.body; 
 
   try {
     const barber = await Barber.findByIdAndUpdate(
       id,
-      { status },
+      { status, availability },
       { new: true }
     );
 
@@ -67,3 +65,4 @@ exports.updateBarberStatus = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
+
